3f00339e0f876e674c5308061cc673d51c1dc4ae
Zay Min Maw <zayminmaw77@gmail.com>
2021-04-19 10:49:32 +0630

    done

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import "../css/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2020 Airbnb clone! No rights reserved!</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+};
+
+export default Footer;
